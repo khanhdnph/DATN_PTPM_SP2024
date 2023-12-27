815642c15ad04f0222b4bc24c353632e883ae3bc
Nam Khanh <duongnamkhanh2001@gmail.com>
2023-12-27 15:01:46 +0700

    DATN

---
+++ src/main/java/com/example/datn_ptpm_sp24/DatnPtpmSp24Application.java
@@ -0,0 +1,13 @@
+package com.example.datn_ptpm_sp24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnPtpmSp24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatnPtpmSp24Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/datn_ptpm_sp24/DatnPtpmSp24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datn_ptpm_sp24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatnPtpmSp24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
